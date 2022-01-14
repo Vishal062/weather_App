4143239f4c21642b6a1bb26c13df6be3bc7e0230
Vishal062 <vs9425348@gmail.com>
2022-01-14 16:13:49 +0530

    Fetch Done

--- src/script.js
+++ src/script.js
@@ -2,24 +2,28 @@
 ApiKey: 0bb32717f46836eb0fad78c557614acd
 Api: api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 */
-const searchCity = document.querySelector('#searchCity');
-searchCity.addEventListener('keypress',setFunc);
+var searchValue = document.querySelector('#searchCity');
+searchValue.addEventListener('keypress',setFunc);
 
 function setFunc(e){
     if(e.keyCode == 13){    //for enter
         //console.log("Hello");
-        getData(searchCity.value);
+        getData(searchValue.value);
     }
 }
 
 function getData(value){
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid={0bb32717f46836eb0fad78c557614acd}`
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=0bb32717f46836eb0fad78c557614acd`;
     fetch(url)
     .then(function (response){
+        console.log(response);
         return response.json()
+        
     })
-    .then(function (weather){
-        document.querySelector('#city').innerHTML = weather.name;
+    .then(function (weather) {
+        console.log(weather);
+        document.querySelector(`#city`).innerHTML = weather.city.name;
+        document.querySelector(`#temp`).innerHTML = weather.list[0].main.temp; //map((e)=> e.main.temp) ;
     })
     .catch(function (err){
         console.log(err);
